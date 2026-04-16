68f90185ccd54e65900a7b6c06730b25c8d0a46d
captainmuffinn <sirui.chen@student.htldornbirn.at>
2026-04-16 10:21:15 +0200

    button Goin

--- Assets/Script/ShowEnterButton.cs
+++ Assets/Script/ShowEnterButton.cs
@@ -4,13 +4,13 @@ public class ShowEnterButton : MonoBehaviour
 {
     public GameObject enterButton;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
             enterButton.SetActive(true);
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
             enterButton.SetActive(false);

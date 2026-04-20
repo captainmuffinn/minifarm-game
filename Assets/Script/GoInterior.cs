75a06692ccb956de368c2c114568432f7a7ce861
captainmuffinn <sirui.chen@student.htldornbirn.at>
2026-04-20 12:59:23 +0200

    Back goin Backto inventor

--- Assets/Script/GoInterior.cs
+++ Assets/Script/GoInterior.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GoInterior : MonoBehaviour
 {
-    public void GoToGameScene()
-    {
-        SceneManager.LoadScene("Interior");
-    }
+        public void GoToInterior()
+        {
+            SceneManager.LoadScene("Interior");
+        }
+
+        public void GoToGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
 }
